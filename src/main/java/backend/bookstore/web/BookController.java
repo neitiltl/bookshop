6916b9ba388c7cdd2b78ca6c1f53771fa2e5f098
Anu <anu.laitila@myy.haaga-helia.fi>
2026-09-10 18:51:55 +0300

    Add Listpage

--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -4,13 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import backend.bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
+
+    private BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/index")
     public String index(Model model) {
 
         return "index";
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+
+        return "booklist";
     }
 
 }
