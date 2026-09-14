0bd9e054f8f6e291aa2ae784c3df51661c5c1f3b
Anu <anu.laitila@myy.haaga-helia.fi>
2026-09-14 16:18:40 +0300

    Add Bootstrap

--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -3,7 +3,10 @@ package backend.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import backend.bookstore.domain.Book;
 import backend.bookstore.domain.BookRepository;
 
 @Controller
@@ -26,6 +29,30 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
 
         return "booklist";
+    }
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        model.addAttribute("book", repository.findById(bookId));
+        return "editbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(Book book) {
+        repository.save(book);
+        return "redirect:booklist";
     }
 
 }
